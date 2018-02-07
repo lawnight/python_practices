6d2e530b7ab6171163ad617b5f9fa129c0af2745
lijiangtao <lijiangtao@digisky.com>
2018-02-07 12:19:14 +0800

    总成交和编码问题

--- funi.py
+++ funi.py
@@ -9,11 +9,11 @@ fileName = 'saleInfo.csv'
 from collections import OrderedDict
 
 request_info = OrderedDict([
-     ('1号楼' , 'e1b28e3e-8d21-423c-9a52-db8bc42a95cf'),
-    ('2号楼' , 'd4ebca50-0191-40e7-8ce1-3e7b72833e1a'),
-    ('3号楼' , 'e09cf1f5-58df-42a8-8aba-0925d8ae5413'),
-    ('4号楼' , 'd85e5e03-94d7-42bb-a685-3a52be7dd05d'),
-    ('5号楼' , '190192e4-7734-44fd-8e53-cfd9e4bfe198')
+     ('1B' , 'e1b28e3e-8d21-423c-9a52-db8bc42a95cf'),
+    ('2B' , 'd4ebca50-0191-40e7-8ce1-3e7b72833e1a'),
+    ('3B' , 'e09cf1f5-58df-42a8-8aba-0925d8ae5413'),
+    ('4B' , 'd85e5e03-94d7-42bb-a685-3a52be7dd05d'),
+    ('5B' , '190192e4-7734-44fd-8e53-cfd9e4bfe198')
     ])
 
 
@@ -58,13 +58,23 @@ def getInfo():
     buildList = obj['data']
 
     info=OrderedDict()
-    info['小区'] = '蓝光T-Max'
+    info['community'] = '蓝光T-Max'
     for key,value in request_info.items():
         value = getValueByKey(value,buildList)
         if value:
             info[key] = value
-    info['时间'] = getTime()
-   
+    
+
+    #总成交数
+    context = requests.get('http://mobileapi.funi.com/m/community/hotSale.json?tal_id=902558776994568&tal=ANDROID&cityId=1')
+    obj = json.loads(context.content.decode('utf-8'))
+    info['totalAmount'] = obj['data']['totalAmount']
+    info['totalArea'] =  obj['data']['totalArea'].encode('utf-8')
+    info['infoDate'] =  obj['data']['date'].encode('utf-8')
+    info['totalPrice'] =  obj['data']['totalPrice'].encode('utf-8')
+
+    info['date'] = getTime()
+
 
     writeCsv(fileName,info)
 
--- map/test.py
+++ map/test.py
@@ -1,17 +1,32 @@
-a = str('已')
-b = str('已')
-import json
-import re
-json_string = json.dumps("解决", ensure_ascii=False).encode('utf8')
+# -*- coding: UTF-8 -*-
+# a = str('已')
+# b = str('已')
+# import json
+# import re
+# json_string = json.dumps("解决", ensure_ascii=False).encode('utf8')
 
-print(json_string)
-print(ord('解'))
+# print(json_string)
+# print(ord('解'))
 
-print('\u89e3\u51b3')
+# print('\u89e3\u51b3')
 
-print(re.findall('[0-9]+', 'fff11fff'))
+# print(re.findall('[0-9]+', 'fff11fff'))
 
 
-te = "['x','y']"
-te = exec('temp = ' + te)
-print(temp[0])
+# te = "['x','y']"
+# te = exec('temp = ' + te)
+# print(temp[0])
+import codecs
+t = "字"
+s = u"字".encode('utf-8')
+if t==s:
+    print 'equal'
+
+
+
+
+
+
+
+
+
